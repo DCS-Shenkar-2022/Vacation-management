aebf41c42577daf5d7caa3ff8f3f7d92345d7255
adi peled <adipeled224@gmail.com>
2022-01-08 13:34:27 +0200

    all work with hooks

--- vacations-project/src/components/formSection/formSection.js
+++ vacations-project/src/components/formSection/formSection.js
@@ -1,24 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Form from '../form/form.js'
 import './formSection.css';
 
-class FormSection extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    renderFormSection() {
-        return (
-            <div className="form-section"> 
-              <Form vacationToEdit={this.props.vacationToEdit} editMode={this.props.editMode} onSave={this.props.funcToUpdate} onCancel={this.props.funcToCancel} onAdd={this.props.funcToAdd} deleteMode={this.props.deleteMode} onAfterDelete={this.props.changeDeleteMode}></Form> 
-            </div>  
-        )
-    }
-
-    render(){
-        return this.renderFormSection();
-    }
+const FormSection = (props) => {
+    return (
+        <div className="form-section">
+            <Form vacationToEdit={props.vacationToEdit} editMode={props.editMode} onSave={props.funcToUpdate} onCancel={props.funcToCancel} onAdd={props.funcToAdd} deleteMode={props.deleteMode} onAfterDelete={props.changeDeleteMode}></Form>
+        </div>
+    )
 }
 export default FormSection;
 
--- vacations-project/src/components/vacationsList/vacationsList.js
+++ vacations-project/src/components/vacationsList/vacationsList.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react';
 import Vacation from '../vacation/vacations';
 import '../vacationsList/vacationsList.css'
 
-class VacationList extends Component {
-    constructor(props) {
-        super(props);
-        this.createOneVacation = this.createOneVacation.bind(this);
-        this.renderVacations = this.renderVacations.bind(this);
-    }
+const VacationList = (props) => {
 
-    createOneVacation(vacation, keyIterator) {
-        return <Vacation key={keyIterator} index={vacation.id} imageUrl={vacation.imageUrl} name={vacation.name} location={vacation.location} price={vacation.price} onDelete={this.props.funcOnDelete} onEdit={this.props.funcOnEdit} onReqEdit={this.props.vacationRequestToEdit}>
+
+    const createOneVacation = (vacation, keyIterator) => {
+        return <Vacation key={keyIterator} index={vacation.id} imageUrl={vacation.imageUrl} name={vacation.name} location={vacation.location} price={vacation.price} onDelete={props.funcOnDelete} onEdit={props.funcOnEdit} onReqEdit={props.vacationRequestToEdit}>
         </Vacation>
     }
 
-    renderVacations(filter) {
+    const renderVacations = (filter) => {
         let resultVacations;
-        let filterVacations = this.props.vacations.filter((vacation) => {
+        let filterVacations = props.vacations.filter((vacation) => {
             if (vacation.name.toLowerCase().includes(filter.toLowerCase()) || vacation.location.toLowerCase().includes(filter.toLowerCase())) {
                 return true;
             }
@@ -24,13 +20,13 @@ class VacationList extends Component {
                 return false;
             }
         });
-        resultVacations = this.props.vacations.map((vacation, keyIterator) => {
+        resultVacations = props.vacations.map((vacation, keyIterator) => {
             if (filter == "") {
-                return this.createOneVacation(vacation, keyIterator);
+                return createOneVacation(vacation, keyIterator);
             }
 
             else if (vacation.name.toLowerCase().includes(filter.toLowerCase()) || vacation.location.toLowerCase().includes(filter.toLowerCase())) {
-                return this.createOneVacation(vacation, keyIterator);
+                return createOneVacation(vacation, keyIterator);
             }
 
         })
@@ -39,17 +35,17 @@ class VacationList extends Component {
             return resultVacations;
         }
         else {
-            return <p> &#9992; Sorry, no search results are found for: "{this.props.filter}" &#128532; </p>
+            return <p> &#9992; Sorry, no search results are found for: "{props.filter}" &#128532; </p>
         }
     }
 
-    render() {
-        return (
-            <div className="vacations-list">
-                {this.renderVacations(this.props.filter)}
-            </div>
-        )
-    }
+
+    return (
+        <div className="vacations-list">
+            {renderVacations(props.filter)}
+        </div>
+    )
+
 }
 
 export default VacationList;
